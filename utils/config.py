2d66b0c231fd1f9ce55940786385d9e2e1abd68b
Austin T <austin.james.tripp@gmail.com>
2019-01-23 16:11:57 -0500

    Added a basic json to layer building config

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,5 @@
 import os
+from torch import nn
 
 def get_expt_name(config_file, configs_dict):
     path, base_name = os.path.split(config_file)
@@ -6,8 +7,27 @@ def get_expt_name(config_file, configs_dict):
     holding_dir = os.path.split(path)[1]
     return os.path.join(holding_dir, base_name)
  
+
 def expt_summary(expt_name, configs):
     barrier_str = "#"*80
     return (barrier_str + "\n\n" + expt_name + "\n\n" + barrier_str
             + "\n\n" + configs.get("desc", "NO DESCRIPTION") + "\n\n")
     
+
+def feed_forward_layer_list(input_size, params):
+    last_size = input_size
+    out = []
+    for e in params:
+        if isinstance(e, int):
+            out.append(nn.Linear(last_size, e)) 
+            last_size = e 
+        elif isinstance(e, str):
+           if e == "BatchNorm1d":
+               out.append(nn.BatchNorm1d(last_size))
+           else:
+               out.append(getattr(nn, e)())
+        else:
+           raise NotImplementedError
+
+    return out 
+
